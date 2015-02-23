a072573f74ed45b021fb9b7d704126bcb006e873
Eric Han <eric.aka.darth_vader@hotmail.com>
2015-02-23 22:37:12 +0800

    Added Start.java and fixed Terminal

--- src/main/java/w094j/run/Start.java
+++ src/main/java/w094j/run/Start.java
@@ -1,20 +1,122 @@
 package w094j.run;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import w094j.ctrl8.database.Database;
+import w094j.ctrl8.display.CLIDisplay;
+import w094j.ctrl8.display.Display;
+import w094j.ctrl8.message.ErrorMessage;
+import w094j.ctrl8.message.OptionsConstants;
+import w094j.ctrl8.pojo.Config;
+import w094j.ctrl8.terminal.Terminal;
+
 /**
  * Class to start the Task Manager.
  * 
- * @author Han Liang Wee, Eric(A0065517A)
+ * @author Chen Tze Cheng(A0112092W)
  */
 public class Start {
 
     /**
      * Runs the Task Manager with some parameters
-     *
+     * 
      * @param args
      *            TODO
      */
-    public static void main(String[] args) {
 
+    // create the logger
+    private static Logger logger = LoggerFactory.getLogger(Start.class);
+    // create the command line parser
+    private static CommandLineParser parser = new GnuParser();
+    // create the Options
+    private static Options optionList = new Options();
+
+    public static void main(String[] args) {
+        logger.info("Starting <Ctrl> + <8>.");
+        // add all existing Options
+        addOptions();
+
+        if (checkArgs(args)) {
+
+            parseArgs(args);
+            Display display = new CLIDisplay();
+            new Terminal(Config.parseArgs(args), display);
+        } else {
+            printHelp();
+        }
+
+    }
+
+    // This will add all the existing options
+    private static void addOptions() {
+        OptionsConstants.addAllOptions();
+        for (int i = 0; i < OptionsConstants.getOptionNumber(); i++) {
+            optionList.addOption(OptionsConstants.getOption(i));
+        }
+    }
+
+    // This is the parser for the args
+    private static void parseArgs(String[] args) {
+        try {
+            // parse the command line arguments
+            CommandLine line = parser.parse(optionList, args);
+
+            // validate that file's Path has been set
+            if (line.hasOption("filePath")) {
+                try {
+                    Database database = new Database(
+                            line.getOptionValue("filePath"));
+                    // print the value of filePath
+                    logger.info("Opening file at "
+                            + line.getOptionValue("filePath"));
+                } catch (Exception e) {
+                    logger.info("Something is wrong with your file");
+                    System.exit(1);
+                }
+            }
+
+            // validate that help has been set
+            if (line.hasOption("help")) {
+                printHelp(optionList);
+            }
+        }
+
+        catch (org.apache.commons.cli.ParseException e) {
+            logger.info(ErrorMessage.OPTION_NOT_FOUND);
+            printHelp(optionList);
+        }
+
+    }
+
+    // check if the args is valid
+    public static boolean checkArgs(String[] args) {
+
+        if (args.length == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void printHelp() {
+        printHelp(optionList);
+    }
+
+    // print the help message
+    private static void printHelp(Options optionList) {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter
+                .printHelp(
+                        "Ctrl-8",
+                        "Welcome to Ctrl-8. Below are the options you can choose",
+                        optionList,
+                        "For more information please refer to https://github.com/cs2103jan2015-w09-4j/main",
+                        true);
     }
 
 }
