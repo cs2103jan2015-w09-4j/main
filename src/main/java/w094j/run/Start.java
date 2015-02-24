42704c5c24a807f1c33c232d71b8d392f86de8b3
tze cheng <imchenchen1994@hotmail.com>
2015-02-24 18:04:34 +0800

    add missingtaskexception dependency, tweak Start

--- src/main/java/w094j/run/Start.java
+++ src/main/java/w094j/run/Start.java
@@ -25,7 +25,8 @@ import w094j.ctrl8.terminal.Terminal;
 public class Start {
 
     /**
-     * Runs the Task Manager with some parameters
+     * Runs the Task Manager with the file path to extract the data from the
+     * file.
      * 
      * @param args
      *            TODO
@@ -36,7 +37,7 @@ public class Start {
     // create the command line parser
     private static CommandLineParser parser = new GnuParser();
     // create the Options
-    private static Options optionList = new Options();
+    private static Options optionList;
 
     public static void main(String[] args) {
         logger.info(NormalMessage.START_MESSAGE);
@@ -58,6 +59,7 @@ public class Start {
 
     // This will add all the existing options
     private static void addOptions() {
+        optionList = new Options();
         OptionsConstants.addAllOptions();
         for (int i = 0; i < OptionsConstants.getOptionNumber(); i++) {
             optionList.addOption(OptionsConstants.getOption(i));
