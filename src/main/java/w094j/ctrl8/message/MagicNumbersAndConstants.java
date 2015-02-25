8ec5a69e7234f23fa59ef3391772d6166a645d59
tze cheng <imchenchen1994@hotmail.com>
2015-02-25 15:18:17 +0800

    add magicnumbers and constants class, issue #58

---
+++ src/main/java/w094j/ctrl8/message/MagicNumbersAndConstants.java
@@ -0,0 +1,11 @@
+package w094j.ctrl8.message;
+
+/**
+ * This class stores all the magic numbers and String constants that will use by
+ * other classes
+ * 
+ * @author A0112092W
+ */
+public class MagicNumbersAndConstants {
+
+}
