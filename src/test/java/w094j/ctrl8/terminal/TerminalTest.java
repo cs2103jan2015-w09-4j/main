387f05080e8dd5af09a5b33c2919091e16ebe11c
rodsonchue <warboi@hotmail.com>
2015-02-26 18:06:13 +0800

    Refactor TerminalBasicHelpTest.java -> TerminalTest.java. Added additional tests.

---
+++ src/test/java/w094j/ctrl8/terminal/TerminalTest.java
@@ -0,0 +1,62 @@
+//@author A0110787A
+package w094j.ctrl8.terminal;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import w094j.ctrl8.display.CLIDisplay;
+import w094j.ctrl8.message.ErrorMessage;
+import w094j.ctrl8.message.NormalMessage;
+import w094j.ctrl8.pojo.Task;
+
+public class TerminalTest {
+    CLIDisplay display;
+    Terminal testTerminal;
+    private String expected;
+    private String output;
+
+    public TerminalTest() {
+        // Setup terminal and a CLIDisplay to go with it
+        this.display = new CLIDisplay();
+        this.testTerminal = new Terminal(this.display);
+    }
+
+    @Test
+    /*
+     * [BASIC] Tests whether the correct exception is thrown when add() is given
+     * a default-constructed task (which is incomplete by definition)
+     */
+    public void basicAddIncompleteTaskTest() {
+        // Expected result
+        this.expected = ErrorMessage.EXCEPTION_IS_INCOMPLETE_TASK;
+
+        // Function to test
+        try {
+            this.testTerminal.add(new Task());
+            ;
+        } catch (Exception e) {
+            assertEquals(this.expected, e.getMessage());
+        }
+
+    }
+
+    @Test
+    /*
+     * [BASIC] Tests whether the help() function is working correctly with
+     * CLIDisplay
+     */
+    public void basicHelpTest() {
+        // Expected result
+        this.expected = NormalMessage.HELP_ALL;
+
+        // Function to test
+        this.testTerminal.help();
+
+        // Output from terminal
+        this.output = this.display.getLastMessage();
+
+        assertEquals(this.expected, this.output);
+    }
+
+}
