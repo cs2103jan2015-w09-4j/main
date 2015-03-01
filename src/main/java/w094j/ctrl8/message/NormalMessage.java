1d3c5da5784cbff222f7649e6e6c0fa317178e4b
rodsonchue <warboi@hotmail.com>
2015-03-01 13:35:12 +0800

    Formated @author to follow requirements. Corrected some text errors.

--- src/main/java/w094j/ctrl8/message/NormalMessage.java
+++ src/main/java/w094j/ctrl8/message/NormalMessage.java
@@ -1,14 +1,12 @@
 package w094j.ctrl8.message;
 
+//@author A0112092W
 /**
  * Class encapsulates all information that is pulled and/or pushed from an
  * external file. Examples include interacting with a local file. Or dumping
  * statement history into an output file. TODO: Cater for Google integration
  */
 
-/**
- * @author Lin Chen-Hsin
- */
 public class NormalMessage {
     public static final String ADD_TASK_SUCCESSFUL = " added successfully!";
     public static final String DELETE_TASK_SUCCESSFUL = " deleted successfully!";
@@ -17,7 +15,7 @@ public class NormalMessage {
     public static final String EXIT_COMMAND = "Thank you for using Ctrl Infinity!";
 
     // @author A0110787A
-    public static final String HELP_ADD_COMMAND_SYNTAX = "add ={<title>} $";
+    public static final String HELP_ADD_COMMAND_SYNTAX = "add ={<title>}";
     public static final String HELP_ALL = NormalMessage.HELP_HEADER
             + NormalMessage.HELP_ADD_COMMAND_SYNTAX + "\n"
             + NormalMessage.HELP_DELETE_COMMAND_SYNTAX + "\n"
@@ -29,11 +27,11 @@ public class NormalMessage {
     public static final String HELP_VIEW_COMMAND_SYNTAX = "view";
 
     public static final String MODIFY_TASK_SUCCESSFUL = " modified successfully!";
-    public static final String NO_TASK_FOUND = "No Tasks found.";
+    public static final String NO_FILEPATH_MESSAGE = "No filepath entered. Starting at default filepath..";
 
-    // @author A0110787A, A0112092W
+    public static final String NO_TASK_FOUND = "No Tasks found.";
+    // @author A0112092W
     public static final String START_MESSAGE = "Starting Ctrl-8...";
     public static final String WELCOME_MESSAGE = "Welcome for using Ctrl-8!";
-    public static final String NO_FILEPATH_MESSAGE = "No filepath entered. Starting at default filepath..";
 
 }
