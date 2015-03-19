77ccfcbbaa161291587244eb9ddd58bbcce2388e
Eric Han <eric.aka.darth_vader@hotmail.com>
2015-03-19 15:03:25 +0800

    Implemented Alias Parsing

---
+++ src/main/java/w094j/ctrl8/parse/AliasParser.java
@@ -0,0 +1,79 @@
+package w094j.ctrl8.parse;
+
+import java.util.Formatter;
+import java.util.Locale;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import w094j.ctrl8.exception.ParseException;
+
+/**
+ * Parser to replace all aliases with the actual string.
+ */
+public class AliasParser {
+
+    /**
+     * Regex to match all Alias, the delimiter is filled at runtime.
+     */
+    private static final String ALIAS_REGEX_FORMAT = "(?<!\\\\)%1$s[\\w]+";
+    private static Logger logger = LoggerFactory.getLogger(AliasParser.class);
+
+    private Map<String, String> aliasMap;
+    private Pattern aliasPattern;
+
+    /**
+     * Creates an Alias Parser with the specified parameters
+     * 
+     * @param aliasData
+     */
+    public AliasParser(AliasData aliasData) {
+        this.aliasMap = aliasData.getAliasMap();
+        StringBuilder sb = new StringBuilder();
+        // Send all output to the Appendable object sb
+        try (Formatter formatter = new Formatter(sb, Locale.getDefault())) {
+            formatter.format(ALIAS_REGEX_FORMAT, Pattern.quote(Character
+                    .toString(aliasData.getAliasCharacter())));
+            this.aliasPattern = Pattern.compile(sb.toString());
+        }
+    }
+
+    /**
+     * Replaces all aliases in inputToReplace to its aliases.
+     *
+     * @param inputToReplace
+     *            the input string to replace aliases.
+     * @return string with all aliases replaced with their actual text.
+     * @throws ParseException
+     *             when the alias cannot be found in the lookup table.
+     */
+    public String replaceAllAlias(String inputToReplace) throws ParseException {
+        Matcher aliasMatcher = this.aliasPattern.matcher(inputToReplace);
+        StringBuilder sb = new StringBuilder();
+        int previousMatchEnd = 0;
+        while (aliasMatcher.find()) {
+            String eaAlias = aliasMatcher.group();
+            String eaAliasReplaced = this.aliasMap.get(eaAlias.substring(1));
+            if (eaAliasReplaced == null) {
+                logger.info(inputToReplace + ": The alias(" + eaAlias
+                        + ") cannot be found.");
+                throw new ParseException("The alias(" + eaAlias
+                        + ") cannot be found.");
+            }
+            logger.debug("Replace Alias(" + eaAlias + ") with \""
+                    + eaAliasReplaced + "\".");
+            sb.append(inputToReplace.substring(previousMatchEnd,
+                    aliasMatcher.start()));
+            previousMatchEnd = aliasMatcher.end();
+
+            sb.append(eaAliasReplaced);
+        }
+        sb.append(inputToReplace.substring(previousMatchEnd));
+        logger.info("\"" + inputToReplace + "\" was parsed to \""
+                + sb.toString() + "\"");
+        return sb.toString();
+    }
+}
---
+++ src/test/java/w094j/ctrl8/parse/AliasParserTest.java
@@ -0,0 +1,113 @@
+package w094j.ctrl8.parse;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import w094j.ctrl8.exception.ParseException;
+
+/**
+ * Test for Alias Parser
+ */
+@RunWith(value = Parameterized.class)
+public class AliasParserTest {
+
+    private Map<String, String> aliasMap;
+    private String expectedReplaced;
+
+    private String inputToReplace;
+    private AliasParser parser;
+
+    /**
+     * Initializes a test with the input to perform the alias replace on and the
+     * expected replaced string.
+     *
+     * @param inputToReplace
+     *            string to perform the replacement operation.
+     * @param expectedReplaced
+     *            expected output after replacement.
+     */
+    public AliasParserTest(String inputToReplace, String expectedReplaced) {
+        this.inputToReplace = inputToReplace;
+        this.expectedReplaced = expectedReplaced;
+    }
+
+    /**
+     * @return test data.
+     */
+    @Parameters(name = "{index}: Parse \"{0}\" to \"{1}\"")
+    //@formatter:off
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                /**
+                 * Normal tests
+                 */
+                {"add ={eat lunch with |eric, |rod, |C and |CH}","add ={eat lunch with Han Liang Wee Eric, Chue Le Sheng Rodson, Tze Cheng and Chen Hsin}"},
+                {"|a ={|e |profHW} -|2","add ={email Prof. Hon Wai} -2pm"},
+                {"add ={eat\\|sleep}","add ={eat\\|sleep}"},
+                /**
+                 * Extreme tests
+                 */
+                {"add ={|C|CH}","add ={Tze ChengChen Hsin}"},
+                {"add ={|CH|C}","add ={Chen HsinTze Cheng}"},
+                {"add ={eat | sleep}","add ={eat | sleep}"},
+                {"add ={eat || sleep}","add ={eat || sleep}"},
+                {"add ={eat ||e| sleep}","add ={eat |email| sleep}"},
+                /**
+                 * Errornous tests
+                 */
+                {"add ={|sleep}",null}
+        });
+    }
+
+    /**
+     * Initializes the lookup table with some values
+     */
+    @Before
+    public void initMap() {
+        this.aliasMap = new HashMap<String, String>();
+        this.aliasMap.put("eric", "Han Liang Wee Eric");
+        this.aliasMap.put("rod", "Chue Le Sheng Rodson");
+        this.aliasMap.put("C", "Tze Cheng");
+        this.aliasMap.put("CH", "Chen Hsin");
+        this.aliasMap.put("a", "add");
+        this.aliasMap.put("e", "email");
+        this.aliasMap.put("profHW", "Prof. Hon Wai");
+        this.aliasMap.put("2", "2pm");
+
+        AliasData data = new AliasData();
+        data.setAliasMap(this.aliasMap);
+        data.setAliasCharacter('|');
+
+        this.parser = new AliasParser(data);
+    }
+
+    // @formatter:on
+
+    /**
+     * Tests the parsing of the command text
+     */
+    @Test
+    public void test() {
+        try {
+            String replacedAlias = this.parser
+                    .replaceAllAlias(this.inputToReplace);
+            assertEquals(this.expectedReplaced, replacedAlias);
+        } catch (ParseException e) {
+            if (this.expectedReplaced == null) {
+                assertTrue(true);
+            } else {
+                assertTrue(false);
+            }
+        }
+    }
+}
