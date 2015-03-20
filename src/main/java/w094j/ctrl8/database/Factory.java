1f5086a68fc7dab0ee0784a28206c6043a34e096
Lin Chen-Hsin <linchenhsin1992@gmail.com>
2015-03-21 00:27:22 +0800

    Added Factory

--- src/main/java/w094j/ctrl8/database/Factory.java
+++ src/main/java/w094j/ctrl8/database/Factory.java
@@ -1,9 +1,66 @@
 package w094j.ctrl8.database;
 
-/**
- * @author Han Liang Wee, Eric(A0065517A)
- * @author Lin Chen-Hsin A0112521B
- */
+import java.io.IOException;
+
+import w094j.ctrl8.pojo.Config;
+import w094j.ctrl8.terminal.Terminal;
+
+//@author A0112521B
+
 public class Factory {
+    private static String DISPLAY = "DISPLAY";
+    private static String PARSER = "PARSER";
+    private static String TASKMANAGER = "TASKMANAGER";
+    private static String TERMINAL = "TERMINAL";
+
+    private Config config = new Config();
+
+    public Factory() {
+        this("");
+    }
+
+    public Factory(String path) {
+        Database database;
+        try {
+            database = new Database(path);
+            this.config = database.getConfig();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * @param type
+     * @return an Object(Display/Parser/TaskManager/Terminal)
+     */
+    public Object create(String type) {
+        if (type.equals(DISPLAY)) {
+            return this.buildDisplay(this.config);
+        } else if (type.equals(PARSER)) {
+            return this.buildParser(this.config);
+        } else if (type.equals(TASKMANAGER)) {
+            return this.buildTaskManager(this.config);
+        } else if (type.equals(TERMINAL)) {
+            return this.buildTerminal(this.config);
+        }
+        return null;
+    }
+
+    private Object buildDisplay(Config config) {
+        return new Display(config);
+    }
+
+    private Object buildParser(Config config) {
+        return new Parser(config);
+    }
+
+    private Object buildTaskManager(Config config) {
+        return new TaskManager(config);
+    }
+
+    private Object buildTerminal(Config config) {
+        return new Terminal(config);
+    }
 
 }
