51f00c1111f1b35022c8d4bb7e78ce093c63e539
Lin Chen-Hsin <linchenhsin1992@gmail.com>
2015-03-21 00:31:51 +0800

    Included AliasData and History in DBfile

--- src/main/java/w094j/ctrl8/pojo/DBfile.java
+++ src/main/java/w094j/ctrl8/pojo/DBfile.java
@@ -7,17 +7,31 @@ import java.util.List;
 
 public class DBfile {
 
+    private List<AliasData> aliasDataList;
     private Config config;
+    private History history;
     private List<Task> taskList;
 
     public DBfile() {
+        this.aliasDataList = new ArrayList<AliasData>();
         this.config = new Config();
+        this.history = new History();
         this.taskList = new ArrayList<Task>();
     }
 
-    public DBfile(List<Task> taskList, Config config) {
-        this.taskList = taskList;
+    public DBfile(List<AliasData> aliasDataList, Config config,
+            History history, List<Task> taskList) {
+        this.aliasDataList = aliasDataList;
         this.config = config;
+        this.history = history;
+        this.taskList = taskList;
+    }
+
+    /**
+     * @return the aliasDataList
+     */
+    public List<AliasData> getAliasDataList() {
+        return this.aliasDataList;
     }
 
     /**
@@ -28,10 +42,25 @@ public class DBfile {
     }
 
     /**
+     * @return the history
+     */
+    public History getHistory() {
+        return this.history;
+    }
+
+    /**
      * @return the taskList
      */
     public List<Task> getTaskList() {
         return this.taskList;
+    }
+
+    /**
+     * @param aliasDataList
+     *            the aliasDataList to set
+     */
+    public void setAliasDataList(List<AliasData> aliasDataList) {
+        this.aliasDataList = aliasDataList;
     }
 
     /**
@@ -40,6 +69,14 @@ public class DBfile {
      */
     public void setConfig(Config config) {
         this.config = config;
+    }
+
+    /**
+     * @param history
+     *            the history to set
+     */
+    public void setHistory(History history) {
+        this.history = history;
     }
 
     /**
