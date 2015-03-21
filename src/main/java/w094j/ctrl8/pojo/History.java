7c2f8f118d5de22a873346fd1248af81dd164361
tze cheng <imchenchen1994@hotmail.com>
2015-03-21 17:48:26 +0800

    add History object

--- src/main/java/w094j/ctrl8/pojo/History.java
+++ src/main/java/w094j/ctrl8/pojo/History.java
@@ -1,5 +1,56 @@
 package w094j.ctrl8.pojo;
 
-public class History {
+import java.util.ArrayList;
 
+import w094j.ctrl8.statement.Statement;
+
+//@author A0112092W
+/**
+ * 
+ * This class is for the action's history in an ArrayList
+ */
+public class History {
+    
+    private ArrayList<Statement> ActionList;
+    
+    /**
+     * Constructor for History Object
+     */
+    public History(){
+        this.ActionList = new ArrayList<Statement>();
+    }
+    
+    /**
+     * return the specific history with an index in the ArrayList
+     * @param index
+     * @return statement
+     */
+    public Statement getHistory(int index){
+        return this.ActionList.get(index);
+    }
+    
+    /**
+     * return the whole history list
+     * @return historyList
+     */
+    public ArrayList<Statement> getHistoryList(){
+        return this.ActionList;
+    }
+    
+    /** add a statement history into the history list
+     * @param history
+     */
+    public void addHistory(Statement history){
+        this.ActionList.add(history);
+    }
+    
+    /**
+     * delete the entire history list
+     */
+    public void deleteHistory(){
+        this.ActionList.clear();
+    }
+    
+    
 }
+
