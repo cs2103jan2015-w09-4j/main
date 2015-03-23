5945aef332bdbee928b1ef3e510ac21446f08798
tze cheng <imchenchen1994@hotmail.com>
2015-03-24 00:24:27 +0800

    errornous and extreme test cases for parameterTest

--- src/test/java/w094j/ctrl8/statement/parameter/ParameterTest.java
+++ src/test/java/w094j/ctrl8/statement/parameter/ParameterTest.java
@@ -11,6 +11,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import w094j.ctrl8.database.config.ParameterConfig;
 import w094j.ctrl8.parse.ParameterParser;
@@ -21,7 +23,7 @@ import w094j.ctrl8.parse.ParameterParser;
  */
 @RunWith(value = Parameterized.class)
 public class ParameterTest {
-
+    private static Logger logger = LoggerFactory.getLogger(ParameterTest.class);
     private static ParameterParser parser;
     private ParameterContainer expectedParamContainer;
     private String input;
@@ -66,8 +68,44 @@ public class ParameterTest {
                  */
                 { "={UROP Meeting} ={UROP Meetings}", Arrays.asList(new Parameter[] {new TitleParameter("UROP Meeting"), new TitleParameter("UROP Meetings")}) },
                 { "={UROP Meeting}#{nus}", Arrays.asList(new Parameter[] {new TitleParameter("UROP Meeting"), new CategoryParameter("nus") }) },
-
-
+                
+                //@author A0112092W 
+                /**
+                 * Errorneous test cases
+                 */
+                //null parameter
+                { "", null},
+                //error parameter
+                {"==", null},
+                //wrong usage of brackets
+                {"=(meeting)",null},
+                //multiple usage of tags in 1 field
+                {"@#{friends}", null},
+                //random parameter
+                {".{today}",null},
+                //random parameter
+                {"*{tmr}", null},
+                //duplicate parameter
+                
+                
+                /**
+                 * Extreme test cases
+                 */
+                //All tags in one time
+                {"={OP2 Meeting} -{tmr 3pm} +{Prepare for own slides} @{biz canteen} %{10} !{tmr 12pm} ~{tmr 2pm} #{cs2101}", 
+                    Arrays.asList(new Parameter[] {new TitleParameter("OP2 Meeting"), new DeadlineParameter("tmr 3pm"),
+                            new DescriptionParameter("Prepare for own slides"),new LocationParameter("biz canteen"),new PriorityParameter("10"),
+                            new ReminderParameter("tmr 12pm"), new StartTimeParameter("tmr 2pm"), new CategoryParameter("cs2101") })
+                },
+                //Extreme multi-tags
+                {"={Meeting1} ={Meeting2} ={Meeting3} ={Meeting4} ={Meeting5} ={Meeting6} ={Meeting7} ={Meeting8} ={Meeting9} ={Meeting10} ={Meeting11}",
+                    Arrays.asList(new Parameter[] {new TitleParameter("Meeting1"),new TitleParameter("Meeting2"),new TitleParameter("Meeting3"),
+                            new TitleParameter("Meeting4"),new TitleParameter("Meeting5"),new TitleParameter("Meeting6"),new TitleParameter("Meeting7"),
+                            new TitleParameter("Meeting8"),new TitleParameter("Meeting9"),new TitleParameter("Meeting10"), new TitleParameter("Meeting11")})
+                    
+                }
+                
+                
         });
     }
 
@@ -87,6 +125,8 @@ public class ParameterTest {
      */
     @Test
     public void test() {
-        assertTrue(this.expectedParamContainer.equals(parser.parse(this.input)));
+
+            assertTrue(this.expectedParamContainer.equals(parser.parse(this.input)));
+        
     }
 }
