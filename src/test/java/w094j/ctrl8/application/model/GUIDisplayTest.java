43456d4cb7a18a300595060814a7de154ff3952c
Eric Han <eric.aka.darth_vader@hotmail.com>
2015-03-25 02:21:11 +0800

    Removed GUI test that opens a new window

--- src/test/java/w094j/ctrl8/application/model/GUIDisplayTest.java
+++ src/test/java/w094j/ctrl8/application/model/GUIDisplayTest.java
@@ -1,25 +1,20 @@
 package w094j.ctrl8.application.model;
 
-import static org.junit.Assert.fail;
-
-import org.junit.Test;
-
-import w094j.ctrl8.display.GUIDisplay;
 
 public class GUIDisplayTest {
 
-    /*
-     * Tests whether application can successfully show itself.
-     */
-    @Test
-    public void BasicDisplayTest() {
-        String[] args = new String[] { "" };
-        try {
-            GUIDisplay guiDisplay = new GUIDisplay(args);
-            assert true;
-        } catch (Exception e) {
-            fail(e.getMessage());
-        }
-
-    }
+// /*
+// * Tests whether application can successfully show itself.
+// */
+// @Test
+// public void BasicDisplayTest() {
+// String[] args = new String[] { "" };
+// try {
+// GUIDisplay guiDisplay = new GUIDisplay(args);
+// assert true;
+// } catch (Exception e) {
+// fail(e.getMessage());
+// }
+//
+// }
 }
