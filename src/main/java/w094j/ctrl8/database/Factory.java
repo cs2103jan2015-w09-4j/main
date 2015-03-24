d4a636571d50a347bd41058e7345d08d96539288
Lin Chen-Hsin <linchenhsin1992@gmail.com>
2015-03-24 21:26:01 +0800

    Modified Factory

--- src/main/java/w094j/ctrl8/database/Factory.java
+++ src/main/java/w094j/ctrl8/database/Factory.java
@@ -1,68 +1,34 @@
 package w094j.ctrl8.database;
 
-import java.io.IOException;
-
-import w094j.ctrl8.database.config.ParserConfig;
 import w094j.ctrl8.parse.Parser;
-import w094j.ctrl8.pojo.Config;
 import w094j.ctrl8.terminal.Terminal;
 
 //@author A0112521B
 
 public class Factory {
-    private static String DISPLAY = "DISPLAY";
-    private static String PARSER = "PARSER";
-    private static String TASKMANAGER = "TASKMANAGER";
-    private static String TERMINAL = "TERMINAL";
-
-    private Config config = new Config();
 
     public Factory() {
-        this("");
+        //initDisplay();
+        //initTaskManager.getInstance();
+        this.initParser();
+        this.initTerminal();
     }
 
-    public Factory(String path) {
-        Database database;
-        try {
-            database = new Database(path);
-            this.config = database.getConfig();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    /**
-     * @param type
-     * @return an Object(Display/Parser/TaskManager/Terminal)
-     */
-    public Object create(String type) {
-// if (type.equals(DISPLAY)) {
-// return this.buildDisplay(this.config);
-// } else if (type.equals(PARSER)) {
-// return this.buildParser(this.config);
-// } else if (type.equals(TASKMANAGER)) {
-// return this.buildTaskManager(this.config);
-// } else if (type.equals(TERMINAL)) {
-// return this.buildTerminal(this.config);
+// private initDisplay() {
+// return Display.getInstance();
 // }
+
+// private TaskManager initTaskManager() {
+// return TaskManager.getInstance();
+// }
+
+    private Parser initParser() {
+        return Parser.getInstance();
+    }
+
+    private Terminal initTerminal() {
+        // return Terminal.getInstance();
         return null;
-    }
-
-// private Object buildDisplay(Config config) {
-// return new Display(config);
-// }
-
-    private Parser buildParser(ParserConfig config) {
-        return new Parser(config);
-    }
-
-// private Object buildTaskManager(Config config) {
-// return new TaskManager(config);
-// }
-
-    private Object buildTerminal(Config config) {
-        return new Terminal(config);
     }
 
 }
